05f328e4654dbea7201b0142316c0e853275782d
koubouhirx7-cloud <koubou.hi.rx7@gmail.com>
2026-04-08 22:45:59 +0900

    fix: add no-store cache headers to prevent Vercel Serverless caching of the customer list

--- api/get-customers.js
+++ api/get-customers.js
@@ -30,6 +30,9 @@ export default async function handler(req, res) {
         }
 
         const data = await response.json();
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
         res.status(200).json(data);
     } catch (error) {
         console.error("Error proxying to MicroCMS:", error);
